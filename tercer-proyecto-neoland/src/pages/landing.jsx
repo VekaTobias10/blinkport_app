3573e4463be1b530a3b315b0fba3f1fc2027ed02
Veronica Tobias <vekatobias@outlook.es>
2021-08-17 23:05:05 +0200

    creación de la app

---
+++ tercer-proyecto-neoland/src/pages/landing.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function LandingPage (){
+   return (
+       <React.Fragment>
+        {/* aqui van nuestros componentes por orden */}
+
+
+       </React.Fragment>
+   );
+}
+
+export default LandingPage;
