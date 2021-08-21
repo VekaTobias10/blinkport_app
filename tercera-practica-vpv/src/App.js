400a14f39630bdcee5e6c62125b06b2db314f682
Veronica Tobias <vekatobias@outlook.es>
2021-08-21 22:48:29 +0200

    Últimos cambios

--- tercera-practica-vpv/src/App.js
+++ tercera-practica-vpv/src/App.js
@@ -1,16 +1,13 @@
 import './App.css';
-import CarrouselContainer from './components/carousel-container';
 import React from 'react';
 import LandingPage from './pages/landing';
-import Header from './header/header'
+
 
 
 function App() {
   return (
     <div className="App">
-       <Header></Header>
-      <CarrouselContainer></CarrouselContainer>
-      <LandingPage></LandingPage>        
+     <LandingPage></LandingPage>        
     </div>
   );
 }
--- tercera-practica-vpv/src/pages/landing.jsx
+++ tercera-practica-vpv/src/pages/landing.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import PriceCardLanding from '../components/landing-components/price-section/price-section';
-
+import Header from '../header/header';
+import CarrouselContainer from '../components/carousel-container';
 
 function LandingPage (){
    return (
        <React.Fragment>
-        {/* aqui van nuestros componentes por orden */}
-
-
+       <Header></Header>
+       <CarrouselContainer></CarrouselContainer>      
        <PriceCardLanding></PriceCardLanding>
        </React.Fragment>
    );
