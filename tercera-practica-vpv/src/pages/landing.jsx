8a0e3761db236aa99fdf879f63843a48436d0ff2
Veronica Tobias <vekatobias@outlook.es>
2021-08-22 13:11:08 +0200

    cambios

--- tercera-practica-vpv/src/pages/landing.jsx
+++ tercera-practica-vpv/src/pages/landing.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import PriceCardLanding from '../components/price-component/price-section';
 import Header from '../components/header-component/header';
 import CarrouselContainer from '../components/carousel-component/carousel-container';
+import ImageSection from '../components/landing-components/home-visual-section';
+
 
 function LandingPage (){
    return (
        <React.Fragment>
        <Header></Header>
+       <ImageSection></ImageSection>
        <CarrouselContainer></CarrouselContainer>    
        <PriceCardLanding></PriceCardLanding>
        </React.Fragment>
