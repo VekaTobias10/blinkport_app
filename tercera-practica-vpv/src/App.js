787872d4efe39dac702886666168b05252e5e763
VirginiaPoglonig <virginiapoglonig@gmail.com>
2021-08-28 20:51:35 +0200

    s

--- tercera-practica-vpv/src/App.js
+++ tercera-practica-vpv/src/App.js
@@ -2,25 +2,8 @@ import './App.css';
 import React from 'react';
 import LandingPage from './pages/landing';
 import ModalPage from './pages/modal';
-<<<<<<< HEAD
-<<<<<<< HEAD
-import RegisterData from './pages/register/index';
-
-
-=======
 import RegisterData from './pages/register/index'
 import "@fontsource/roboto";
->>>>>>> main
-=======
-
-import RegisterData from './pages/register/index';
-
-
-
-
-import "@fontsource/roboto";
-
->>>>>>> ba42ad91ef1472af59c2f11e23c4e28d89e3a205
 import {
   BrowserRouter as Router,
   Switch,
